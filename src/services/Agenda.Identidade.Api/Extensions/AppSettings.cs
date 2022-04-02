82aa01d3883b1476c21b36dd63d87f8a2c635c45
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2022-04-01 23:56:41 -0300

    Resposta do login com o JWT

--- src/services/Agenda.Identidade.Api/Extensions/AppSettings.cs
+++ src/services/Agenda.Identidade.Api/Extensions/AppSettings.cs
@@ -3,7 +3,7 @@
 public class AppSettings
 {
     public string Secret { get; set; }
-    public string ExpiracaoHoras { get; set; }
+    public int ExpiracaoHoras { get; set; }
     public string Emissor { get; set; }
     public string ValidoEm { get; set; }
 }
