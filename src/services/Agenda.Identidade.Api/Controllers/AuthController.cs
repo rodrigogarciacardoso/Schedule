5fe1f17d27c0668a3061dca45a80aadf592ca1ed
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2022-04-01 22:59:29 -0300

    Adicionado migration do identity e controller basica de autenticação

---
+++ src/services/Agenda.Identidade.Api/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Agenda.Identidade.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agenda.Identidade.Api.Controllers;
+
+[Route("api/identidades")]
+public class AuthController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    [Route("nova-conta")]
+    public async Task<IActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+    {
+        if (ModelState.IsValid == false) return BadRequest();
+
+        var user = new IdentityUser()
+        {
+            UserName = usuarioRegistro.Email,
+            Email = usuarioRegistro.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, false);
+            return Ok();
+        }
+
+        return BadRequest();
+    }
+
+    [HttpPost]
+    [Route("autenticar")]
+    public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+    {
+        if (ModelState.IsValid == false) return BadRequest();
+
+        var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, 
+            false, true);
+
+        if (result.Succeeded)
+        {
+            return Ok();
+        }
+
+        return BadRequest();
+    }
+}
--- src/services/Agenda.Identidade.Api/Program.cs
+++ src/services/Agenda.Identidade.Api/Program.cs
@@ -1,6 +1,18 @@
+using Agenda.Identidade.Api.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +31,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
